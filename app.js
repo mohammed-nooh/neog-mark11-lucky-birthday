6129a47fc903c22ce31d0bf5a15bfeb1b568a299
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-28 09:41:02 +0530

    update app.js

--- app.js
+++ app.js
@@ -4,26 +4,30 @@
 const privacyNotice=document.querySelector("#privacy-notice");
 const closeBtn=document.querySelector(".close-btn");
 const dateElement=document.querySelector("#date");
-const luckyElement=document.querySelector(".input-field");
+const luckyElement=document.querySelector("#lucky-number");
 const checkBtn=document.querySelector("#check");
 const message=document.querySelector(".message");
-const imageElement=document.querySelector("#image-section");
+const imageElement=document.querySelector("#image");
+const form=document.querySelector("#form");
 
 
 //functions
 
-function checkHandler(){
+function checkHandler(e) {
+    e.preventDefault();
+    // console.log("hi iam nooh and my program is not working");
     let sum=0;
     const date=dateElement.value.replace(/-/g,"");
-    console.log(date);
+    // console.log(`date is ${date}`);
     const luckyNumber=luckyElement.value;
+    // console.log(`luckyNumber is ${luckyNumber}`);
     const array= [...date];
     // console.log(array);
     for(let i of array){
         sum+=Number(i);
     }
     // console.log(sum);
-    if(sum % luckyNumber ==0){
+    if((sum % luckyNumber) ===0){
         messageHandler("YAY you are lucky");
         imageElement.src="images/happy.svg";
     }else{
@@ -44,4 +48,5 @@ function messageHandler(text){
 
 closeBtn.addEventListener("click",closeHandler);
 
-checkBtn.addEventListener("click",checkHandler);
+
+form.addEventListener("submit",checkHandler);
